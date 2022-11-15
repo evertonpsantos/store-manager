77980d2bd62e0c12d6ce07cf24d2a5543f8680b7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-11-14 22:39:03 -0300

    Corrigindo respostas do DB

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -17,7 +17,7 @@ describe('Tests the products model layer', () => {
   });
 
   it('Tests if the right product is returned when on /:id route', async () => {
-    sinon.stub(connection, 'execute').resolves([singleProductResponse]);
+    sinon.stub(connection, 'execute').resolves([[singleProductResponse]]);
     const result = await productsModel.findById(1);
     expect(result).to.be.deep.equal(singleProductResponse);
   });
@@ -25,7 +25,7 @@ describe('Tests the products model layer', () => {
   it('Tests if it possible to insert a new product in the DB', async () => {
     sinon.stub(connection, 'execute')
       .onFirstCall().resolves([{ insertId: 1 }])
-      .onSecondCall().resolves([newRegisteredProductMock]);
+      .onSecondCall().resolves([[newRegisteredProductMock]]);
     
     const result = await productsModel.createNewProduct(newProduct);
     expect(result).to.be.deep.equal(newRegisteredProductMock);
